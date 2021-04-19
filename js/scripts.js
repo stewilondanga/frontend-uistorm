e6722c13b4c01d35bd081d5d56b20cc680245ef7
Stewart ilondanga <stilondanga@gmail.com>
2021-04-19 13:37:00 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ let currSec;
 
 function inView(el) {
   var rect = el.getBoundingClientRect();
-  /*
-    return (
-      rect.bottom > 0 &&
-      rect.right > 0 &&
-      rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
-      rect.top < (window.innerHeight || document.documentElement.clientHeight));
 
-  }
+  return (
+    rect.bottom > 0 &&
+    rect.right > 0 &&
+    rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+    rect.top < (window.innerHeight || document.documentElement.clientHeight));
 
+}
+/*
   function onVisibilityChange(el) {
     let old_visible;
     let visible = inView(el);
